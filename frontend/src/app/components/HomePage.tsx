c3549814896262b6ec4f804f9ddef42345279796
Courtney Adams <courtneykadams7@gmail.com>
2025-02-08 13:54:03 -0500

    home page

--- frontend/src/app/components/HomePage.tsx
+++ frontend/src/app/components/HomePage.tsx
@@ -12,7 +12,17 @@ export default function HomePage() {
     return (
         <section className={style.bg}>
             <div className={style.text}>
-                <h1 className={style.h1}>Welcome</h1>
+                <h2 className={style.h2}>Welcome To</h2>
+                <h1 className={style.h1}>The Consonance</h1>
+                <h3 className={style.h3}>Your musical journey starts here! Our app uses AI to generate 
+                    the perfect chord progression based on your mood, genre, or any other input you 
+                    provide. Once you’ve selected your preferences, we’ll display a custom chord 
+                    progression and even take it a step further by finding songs that feature that same 
+                    progression. To top it off, we’ll create a personalized playlist just for you, so 
+                    you can explore tracks that match your vibe. Whether you're creating your own music 
+                    or just looking for fresh tunes, The Consonance brings the power of music and AI together 
+                    in one easy-to-use platform!</h3>
+
             </div>
             <button onClick={getStarted} className={style.button}>Get Started</button>
         </section>
