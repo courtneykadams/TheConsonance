b7176682ca918aae4d04908a121d6f6548903983
Courtney Adams <courtneykadams7@gmail.com>
2025-02-09 02:47:04 -0500

    UI

--- frontend/src/app/components/Search.tsx
+++ frontend/src/app/components/Search.tsx
@@ -1,17 +1,32 @@
 "use client"
+import { useState } from "react";
 import style from "./Search.module.css";
 
 export default function Search() {
 
-    return (
-        <div className={style.search}>
-            <span className={style.searchIcon}>search</span>
-            <input
-                type="text"
-                placeholder="Enter a mood or genre to begin"
-                className={style.searchBar}
-            />      
+    const [searchInput, setSearchInput] = useState("");
 
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(event.target.value);
+    };
+
+    const generate = () => {
+        console.log(searchInput);
+    }
+
+    return (
+        <div className={style.searchSection}>
+            <div className={style.search}>
+                <span className={style.searchIcon}>search</span>
+                <input
+                    type="text"
+                    placeholder="Enter a mood or genre to begin"
+                    className={style.searchBar}
+                    value={searchInput} 
+                    onChange={handleInputChange}
+                /> 
+            </div>
+            <button onClick={generate} className={style.button}>Generate</button>
         </div>
     );
 }
